3d0d6816b69a3d2c579db058d122471f87de031d
kendi mbobua <laurencemmarete@gmail.com>
2023-12-13 19:12:11 +0300

    first commit

---
+++ store/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)    
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+ 
+#order object ,customer can have many orders    
+class Order(models.Model):  
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null = True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems =self.orderitem_set.all()
+        total= sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems =self.orderitem_set.all()
+        total= sum([item.quantity for item in orderitems])
+        return total
+
+#model will need a product attribute connected to the product model,the order this item is  connected to ,
+#  quantity  and date this item was added to cart. 
+class OrderItem(models.Model):  
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+# Shipping address  model.This model will be a child to order and will only be created if at least one orderitem
+# within an order is a physical product(if product.digital == False).
+# also connect this model to a customer so that a customer can reuse this shipping adddress if needed in future
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
+    
